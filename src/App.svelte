917f9102dda16864de8b752ef394b63913e77219
yuyosy <y-ysss@outlook.com>
2022-07-11 01:20:52 +0900

    Update(src/App.svelte): update

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,12 @@
-<script lang="ts"></script>
-<main>
-HelloNewTab
-</main>
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import {getBookmarkItems} from './getBookmarks'
+
+    onMount(async () => {
+        let bookmarks = await chrome.bookmarks.getTree();
+        const results = getBookmarkItems(bookmarks)
+        console.log(results)
+    });
+</script>
+
+<main>HelloNewTab</main>
