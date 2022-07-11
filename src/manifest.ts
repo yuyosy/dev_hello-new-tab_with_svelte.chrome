ae23d2abfe2bf2887cfe4f30d837c3e1e692ab13
yuyosy <y-ysss@outlook.com>
2022-07-12 00:32:42 +0900

    Add: src/manifest.ts

---
+++ src/manifest.ts
@@ -0,0 +1,30 @@
+import type {ManifestV3Export} from "@crxjs/vite-plugin";
+
+
+import pkgJson from "../package.json";
+
+const manifest: ManifestV3Export = {
+    manifest_version: 3,
+    name: 'Hello-NewTab',
+    version: pkgJson.version,
+    description: '__MSG_app_description__',
+    incognito: 'split',
+    default_locale: 'en',
+    icons: {
+        '16': 'icons/icon-016.png',
+        '48': 'icons/icon-016.png',
+        '128': 'icons/icon-016.png'
+    },
+    permissions: [
+        'bookmarks',
+        'chrome://favicon/'
+    ],
+    chrome_url_overrides: {
+        newtab: 'src/newtab.html'
+    },
+    content_security_policy: {
+        extension_pages: "script-src 'self'; object-src 'self'"
+    }
+}
+
+export {manifest}
