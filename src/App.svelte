0c88766e661af177c42656d148b24bca793474b5
yuyosy <y-ysss@outlook.com>
2022-07-12 00:35:21 +0900

    Update(src/App.svelte): add bookmark element

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,35 @@
 <script lang="ts">
-    import { onMount } from 'svelte';
-    import {getBookmarkItems} from './getBookmarks'
+    import { getBookmarkItems } from "./getBookmarks";
 
-    onMount(async () => {
-        let bookmarks = await chrome.bookmarks.getTree();
-        const results = getBookmarkItems(bookmarks)
-        console.log(results)
-    });
+    const getbookmark = async ():Promise<Folder[]> => {
+        let bookmarks: chrome.bookmarks.BookmarkTreeNode[] = await chrome.bookmarks.getTree();
+        let results: Folder[] = getBookmarkItems(bookmarks);
+        return results
+    };
+    let promise = getbookmark()
 </script>
 
 <main>HelloNewTab</main>
+
+{#await promise}
+    <p>...waiting</p>
+{:then results}
+    {#each results as folder}
+        <div>
+            <div class="content-header">{folder.title}</div>
+            <ul>
+                {#each folder.children as bookmark} 
+                <li>
+                    <a title={bookmark.title} href={bookmark.url} >
+                        <!-- <img class="favicon" src={bookmark.faviconUrl} alt={bookmark.title} style="border-radius: 50%;"/> -->
+                        {bookmark.title}
+                    </a>
+                </li>
+                {/each}
+                <span class="bookmark-count">19 bookmarks</span>
+            </ul>
+        </div>
+    {/each}
+{:catch error}
+    <p style="color: red">{error.message}</p>
+{/await}
