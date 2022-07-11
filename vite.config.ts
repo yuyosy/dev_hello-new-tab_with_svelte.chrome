7cf7fc21792b976a1615ed15c320d279d54b72b4
yuyosy <y-ysss@outlook.com>
2022-07-12 00:33:29 +0900

    Update(vite.config.ts): add build option

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,15 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { crx } from "@crxjs/vite-plugin";
 import preprocess from "svelte-preprocess";
 
-import manifest from "./public/manifest.json";
+import {manifest} from "./src/manifest";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     svelte({preprocess: preprocess()}),
     crx({ manifest: manifest })
-  ]
+  ],
+  build: {
+    sourcemap: true
+  }
 })
